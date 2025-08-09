678c1a188bfa8f313f474e20f775c3c589718b58
Antarcticpx <antarcticpx@gmail.com>
2025-08-10 02:11:24 +0700

    style : edit card width

--- components/shared/TeamCard.tsx
+++ components/shared/TeamCard.tsx
@@ -17,10 +17,10 @@ const TeamCard = () => {
     //     {/* <div className="absolute inset-[-2px] rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 -z-10" /> */}
     // </div>
    
-    <div className="relative w-44 md:w-52 flex flex-col items-center p-5  bg-zinc-100 dark:bg-zinc-900 rounded-lg transform transition duration-300 ">
+    <div className="relative w-3/4 md:w-52 flex flex-col items-center p-5  bg-zinc-100 dark:bg-zinc-900 rounded-lg transform transition duration-300 ">
         <div className="absolute inset-0 z-[-1] rounded-lg bg-gradient-to-tr from-blue-600 to-purple-600 blur-md opacity-75"></div>
         
-        <div className="w-40 h-56 md:w-48 md:h-64 relative rounded-lg overflow-hidden z-0">
+        <div className="w-full h-56 md:w-48 md:h-64 relative rounded-lg overflow-hidden z-0">
             <Image
                 src="/images/team.jpg"
                 alt="team photo"
@@ -32,7 +32,7 @@ const TeamCard = () => {
                 <span>Product Manager</span>
             </div>
         </div>
-        <h3 className="mt-4 text-primary text-xl font-bold">Firgan</h3>
+        <h3 className="mt-4 text-primary text-xl md:text-sm md:text-center font-bold">Muhammad Firgan</h3>
     </div>
   );
 };
